0b3a9e69b45f46a6719e4c6a91dedb47a8072bd0
unknown <vdsabev@.(none)>
2011-08-10 18:38:15 +0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mishmash::Application.config.secret_token = '00f4389a78dd55dccc1ba9c5139b5bb02cc78922f469154cc986b5735fcac6b5b4a8eee5496d3a97cc6c46612bea7a5e8cec4c4428c6cd911d5e0d5b8822ead3'
