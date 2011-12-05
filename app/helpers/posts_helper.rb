793edcdb5593eb0acc8bd0da453661999b9078f2
unknown <vdsabev@.iwizards.eu>
2011-12-05 10:53:15 +0200

    Inserting of URLs.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,13 +2,16 @@ module PostsHelper
   TRUNC_POST_LENGTH = 200
   MAX_WORD_WIDTH = 43
   ZERO_WIDTH_SPACE = '&#8203;'
-  IMAGE_URL_REGEX = /(?:([^:\/?#]+):)?(?:\/\/([^\/?#]*))?([^?#]*\.(?:jpg|gif|png))(?:\?([^#]*))?(?:#(.*))?/
+  URL_REGEX = /^(http|ftp|https):\/\/(.*)$/i
+  IMAGE_URL_REGEX = /(>{1})((http|ftp|https):\/\/(.*)\.(?:jpg|jpeg|gif|png))(<\/a>){1}/i
+  # IMAGE_URL_REGEX = /(?:([^:\/?#]+):)?(?:\/\/([^\/?#]*))?([^?#]*\.(?:jpg|gif|png))(?:\?([^#]*))?(?:#(.*))?/
   
   def format_post(content)
     raw_content = content.gsub(/\n/, '<br>').gsub(/@(\w+)/, link_to('@\\1', users_path + '/\\1'))
     split_content = raw_content.split.map{ |s| wrap_long_string(s) }.join(' ')
-    insert_image = split_content.gsub(IMAGE_URL_REGEX, '<img src="\\0" />')
-    formatted_content = simple_format(insert_image)
+    insert_urls = split_content.gsub(URL_REGEX, '<a href="\\0">\\0</a>')
+    insert_images = insert_urls.gsub(IMAGE_URL_REGEX, '><img src="\\2" /></a>')
+    formatted_content = simple_format(insert_images)
   end
   
   private
