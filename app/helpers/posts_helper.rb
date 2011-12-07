c68a24a0bb435fd061c744f06f1d2e75acb49100
unknown <vdsabev@.iwizards.eu>
2011-12-07 11:59:40 +0200

    Regex fixes. Wrapping long words.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,22 +1,15 @@
 module PostsHelper
   TRUNC_POST_LENGTH = 200
-  MAX_WORD_WIDTH = 43
-  ZERO_WIDTH_SPACE = '&#8203;'
+  NEWLINE_REGEX = /\n/
+  WHITESPACE_REGEX = /\s+/
   URL_REGEX = /(http|ftp|https):\/\/(\S*)/i
   IMAGE_URL_REGEX = /(>{1})((http|ftp|https):\/\/(\S*)\.(jpg|jpeg|gif|png)(\?[^\\\/\s]+)?)(<\/a>){1}/i
   
   def format_post(content)
-    raw_content = content.gsub(/\n/, '<br>').gsub(/@(\w+)/, link_to('@\\1', users_path + '/\\1'))
-    split_content = raw_content.split.map{ |s| wrap_long_string(s) }.join(' ')
-    insert_urls = split_content.gsub(URL_REGEX, '<a class="_blank" href="\\0">\\0</a>')
+    insert_newlines_and_whitespaces = content.gsub(NEWLINE_REGEX, "<br>").gsub(WHITESPACE_REGEX, " ")
+    insert_user_paths = insert_newlines_and_whitespaces.gsub(AT_USERNAME_REGEX, link_to("@\\1", users_path + "/\\1") + "\\2")
+    insert_urls = insert_user_paths.gsub(URL_REGEX, '<a class="_blank" href="\\0">\\0</a>')
     insert_images = insert_urls.gsub(IMAGE_URL_REGEX, '><img src="\\2" /></a>')
-    formatted_content = simple_format(insert_images)
+    format_content = simple_format(insert_images)
   end
-  
-  private
-    def wrap_long_string(text, max_width = MAX_WORD_WIDTH)
-      text # dummy for now, to avoid mangling URLs
-      # regex = /.{1,#{max_width}}/
-      # (text.length < max_width) ? text : text.scan(regex).join(ZERO_WIDTH_SPACE)
-    end
 end
--- config/initializers/global_constants.rb
+++ config/initializers/global_constants.rb
@@ -1,2 +1,3 @@
-USERNAME_REGEX = /^[a-z0-9]+[\w-]*[a-z0-9]$/i # Starts and ends with a letter or a digit, has letters, digits, hyphens or underscores in between
+USERNAME_REGEX = /^[a-z0-9]+[-\w]+[a-z0-9]+$/i # Starts and ends with a letter or a digit, has letters, digits, hyphens or underscores in between
+AT_USERNAME_REGEX = /@([a-z0-9]+[-\w]+[a-z0-9]+)(\W*)/i # Based on USERNAME_REGEX; starts with @ and can end with a punctuation mark
 EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
