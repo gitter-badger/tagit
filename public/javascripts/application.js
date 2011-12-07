ee3bf0400eb463b2d1454948a199eb112ef8a04d
unknown <vdsabev@.(none)>
2011-12-07 22:32:26 +0200

    Improved client flash.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,11 +2,22 @@
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ajaxComplete(function(event, request) {
   var error = request.getResponseHeader('X-Error-Message');
-  if (error) alert(error);
-
+  if (error) showFlash(error, 'error');
+  
   var warning = request.getResponseHeader('X-Warning-Message');
-  if (warning) alert(warning);
-
+  if (warning) alert(warning, 'warning');
+  
   var notice = request.getResponseHeader('X-Notice-Message');
-  if (notice) alert(notice);
+  if (notice) alert(notice, 'notice');
 });
+
+function showFlash(message, type) {
+  var close_button = '<span id="close_client_flash" class="delete_button">×</span>';
+  var client_flash = $('#client_flash');
+  client_flash.html(message + close_button);
+  client_flash.attr('class', type);
+  $('#close_client_flash').bind('click.flash', function() {
+    client_flash.attr('class', null);
+    client_flash.empty();
+  });
+}
