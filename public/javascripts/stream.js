b0230013030a834f7c37f830d9bd0facd7a5c496
unknown <vdsabev@.iwizards.eu>
2011-12-08 13:34:11 +0200

    Improved video embedding a tad more.

--- public/javascripts/stream.js
+++ public/javascripts/stream.js
@@ -12,11 +12,11 @@ function embedVideo() {
 }
 
 function clickVideo(event) {
-  $(this).off('click.video'); //There is a 4px region on the bottom of the div that can still be clicked
+  $(this).off('click.video'); //There is a 4px region on the bottom of the div that can still be clicked, so turn the handler off
   $(this).html('<iframe width="455" height="338" src="http://youtube.com/embed/' + event.data.src + '?autoplay=1" frameborder="0" allowfullscreen></iframe>');
   var self = this;
-  $('.video').each(function(index) {
-    if (this != self) {
+  $('.video').each(function() {
+    if (this != self && $(this).children('iframe').length > 0) { //That's just one
       $(this).on('click.video', { src: event.data.src }, clickVideo); //Attach the handler for other videos back on
       $(this).html('<img src="http://img.youtube.com/vi/' + event.data.src + '/0.jpg" /><div class="play_video"></div>');
     }
