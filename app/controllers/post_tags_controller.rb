839034b9d5ab5e3a9c47aad6f26c7b076a9a845d
unknown <vdsabev@.iwizards.eu>
2011-12-16 18:59:37 +0200

    Recommit.

--- app/controllers/post_tags_controller.rb
+++ app/controllers/post_tags_controller.rb
@@ -2,16 +2,24 @@ class PostTagsController < ApplicationController
   respond_to :html, :js
   
   def create
-    @post = Post.find(params[:post_tag][:post_id])
-    @tag = Tag.find(params[:post_tag][:tag_id])
+    # flash[:notice] = params.map{ |p| p.to_s }.join(", ")
+    # redirect_to root_path
+    
+    @post = Post.find(params[:post_id])
+    @tag = Tag.find(params[:tag_id])
     @post.tag!(@tag, current_user)
     respond_with @post, @tag
   end
   
   def destroy
-    @post = Post.find(params[:post_tag][:post_id])
-    @tag = Tag.find(params[:post_tag][:tag_id])
-    @post.untag!(@tag, current_user)
+    post_tag = PostTag.find_by_id(params[:id])
+    if post_tag.nil?
+      flash[:error] = t(:record_not_found_message)
+      return
+    end
+    @post = post_tag.post
+    @tag = post_tag.tag
+    post_tag.destroy
     respond_with @post, @tag
   end
 end
