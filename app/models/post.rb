605b895c9feef9d4f9c07498e0ccd8565b2eaee8
unknown <vdsabev@.(none)>
2012-02-05 21:47:19 +0200

    Some fixes for PostgreSQL.

--- app/models/post.rb
+++ app/models/post.rb
@@ -48,16 +48,17 @@ class Post < ActiveRecord::Base
   
   def tags_for_user(user) # Select ([post tags] for [this user]) + ([post tags] for [author and followed users] except those this user already has)
     post_tags
-      .select('DISTINCT ([post_tags].id), [tags].name, [post_tags].*')
-      .joins('JOIN [posts] ON [post_tags].post_id = [posts].id')
-      .joins('JOIN [tags] ON [post_tags].tag_id = [tags].id')
-      .joins('LEFT JOIN [relationships] ON [post_tags].user_id = [relationships].followed_id')
-      .where('[post_tags].user_id = ? OR (([post_tags].user_id = [posts].user_id OR [relationships].follower_id = ?) AND [post_tags].tag_id NOT IN (SELECT [post_tags].tag_id FROM [post_tags] WHERE [post_tags].post_id = [posts].id AND [post_tags].user_id = ?))', user.id, user.id, user.id)
-      .order('[tags].name ASC')
+      .select('"tags".name, "post_tags".*')
+      .joins('JOIN "posts" ON "post_tags".post_id = "posts".id')
+      .joins('JOIN "tags" ON "post_tags".tag_id = "tags".id')
+      .joins('LEFT JOIN "relationships" ON "post_tags".user_id = "relationships".followed_id')
+      .where('"post_tags".user_id = ? OR (("post_tags".user_id = "posts".user_id OR "relationships".follower_id = ?) AND "post_tags".tag_id NOT IN (SELECT "post_tags".tag_id FROM "post_tags" WHERE "post_tags".post_id = "posts".id AND "post_tags".user_id = ?))', user.id, user.id, user.id)
+      .order('"tags".name ASC')
+      .group('"post_tags".id')
   end
   
   def is_collapsed_by(user)
-    user.settings.collapse_posts == 'true' || (!user.settings.collapsed_posts.nil? && user.settings.collapsed_posts.include?(id))
+    user.settings.collapse_posts == "true" || (!user.settings.collapsed_posts.nil? && user.settings.collapsed_posts.include?(id))
   end
   
   def self.from_followed_users(user)      
@@ -66,10 +67,10 @@ class Post < ActiveRecord::Base
   
   def self.from_user_stream(user) # Select [posts] from [this user + followed users + tagged posts] that have [tags this user has not blocked]
     Post
-      .select('DISTINCT ([posts].id), [posts].*')
-      .joins('LEFT JOIN [relationships] ON [posts].user_id = [relationships].followed_id')
-      .joins('LEFT JOIN [post_tags] ON [posts].id = [post_tags].post_id')
-      .joins('LEFT JOIN [user_tags] ON [post_tags].tag_id = [user_tags].tag_id')
-      .where('([posts].user_id = ? OR [relationships].follower_id = ? OR [post_tags].user_id = ?) AND [user_tags].tag_id IS NULL', user.id, user.id, user.id)
+      .select('DISTINCT (posts.id), "posts".*')
+      .joins('LEFT JOIN "relationships" ON "posts".user_id = "relationships".followed_id')
+      .joins('LEFT JOIN "post_tags" ON "posts".id = "post_tags".post_id')
+      .joins('LEFT JOIN "user_tags" ON "post_tags".tag_id = "user_tags".tag_id')
+      .where('("posts".user_id = ? OR "relationships".follower_id = ? OR "post_tags".user_id = ?) AND "user_tags".tag_id IS NULL', user.id, user.id, user.id)
   end
 end
