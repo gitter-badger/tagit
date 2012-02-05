6b23864283824b1753153e9bbb4e292a136c9a6b
unknown <vdsabev@.(none)>
2012-02-05 21:36:08 +0200

    Some fixes for PostgreSQL.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   attr_accessible :title, :content, :tag_list
   
   belongs_to :user
-  has_many :post_tags, :class_name => "PostTag", :foreign_key => "post_id", :dependent => :destroy
+  has_many :post_tags, :class_name => 'PostTag', :foreign_key => 'post_id', :dependent => :destroy
   has_many :tags, :through => :post_tags, :source => :tag
   
   validates :title,
@@ -14,14 +14,14 @@ class Post < ActiveRecord::Base
   validates :user_id,
     :presence => true
   
-  default_scope :order => "created_at DESC"
+  default_scope :order => 'created_at DESC'
   
   def self.per_page
     10
   end
   
   def tag_list(uid = user_id)
-    post_tags.find_all_by_user_id(uid).map{ |post_tag| post_tag.tag.name }.join(", ")
+    post_tags.find_all_by_user_id(uid).map{ |post_tag| post_tag.tag.name }.join(', ')
   end
   
   def tag_with_list(list, user, destroy_old = true)
@@ -48,16 +48,16 @@ class Post < ActiveRecord::Base
   
   def tags_for_user(user) # Select ([post tags] for [this user]) + ([post tags] for [author and followed users] except those this user already has)
     post_tags
-      .select('DISTINCT (post_tags.id), "tags".name, "post_tags".*')
-      .joins('JOIN "posts" ON "post_tags".post_id = "posts".id')
-      .joins('JOIN "tags" ON "post_tags".tag_id = "tags".id')
-      .joins('LEFT JOIN "relationships" ON "post_tags".user_id = "relationships".followed_id')
-      .where('"post_tags".user_id = ? OR (("post_tags".user_id = "posts".user_id OR "relationships".follower_id = ?) AND "post_tags".tag_id NOT IN (SELECT "post_tags".tag_id FROM "post_tags" WHERE "post_tags".post_id = "posts".id AND "post_tags".user_id = ?))', user.id, user.id, user.id)
-      .order('"tags".name ASC')
+      .select('DISTINCT ([post_tags].id), [tags].name, [post_tags].*')
+      .joins('JOIN [posts] ON [post_tags].post_id = [posts].id')
+      .joins('JOIN [tags] ON [post_tags].tag_id = [tags].id')
+      .joins('LEFT JOIN [relationships] ON [post_tags].user_id = [relationships].followed_id')
+      .where('[post_tags].user_id = ? OR (([post_tags].user_id = [posts].user_id OR [relationships].follower_id = ?) AND [post_tags].tag_id NOT IN (SELECT [post_tags].tag_id FROM [post_tags] WHERE [post_tags].post_id = [posts].id AND [post_tags].user_id = ?))', user.id, user.id, user.id)
+      .order('[tags].name ASC')
   end
   
   def is_collapsed_by(user)
-    user.settings.collapse_posts == "true" || (!user.settings.collapsed_posts.nil? && user.settings.collapsed_posts.include?(id))
+    user.settings.collapse_posts == 'true' || (!user.settings.collapsed_posts.nil? && user.settings.collapsed_posts.include?(id))
   end
   
   def self.from_followed_users(user)      
@@ -66,10 +66,10 @@ class Post < ActiveRecord::Base
   
   def self.from_user_stream(user) # Select [posts] from [this user + followed users + tagged posts] that have [tags this user has not blocked]
     Post
-      .select('DISTINCT (posts.id), "posts".*')
-      .joins('LEFT JOIN "relationships" ON "posts".user_id = "relationships".followed_id')
-      .joins('LEFT JOIN "post_tags" ON "posts".id = "post_tags".post_id')
-      .joins('LEFT JOIN "user_tags" ON "post_tags".tag_id = "user_tags".tag_id')
-      .where('("posts".user_id = ? OR "relationships".follower_id = ? OR "post_tags".user_id = ?) AND "user_tags".tag_id IS NULL', user.id, user.id, user.id)
+      .select('DISTINCT ([posts].id), [posts].*')
+      .joins('LEFT JOIN [relationships] ON [posts].user_id = [relationships].followed_id')
+      .joins('LEFT JOIN [post_tags] ON [posts].id = [post_tags].post_id')
+      .joins('LEFT JOIN [user_tags] ON [post_tags].tag_id = [user_tags].tag_id')
+      .where('([posts].user_id = ? OR [relationships].follower_id = ? OR [post_tags].user_id = ?) AND [user_tags].tag_id IS NULL', user.id, user.id, user.id)
   end
 end
