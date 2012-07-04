e1be5dd0d2cfb55fbef55b7fbc74665c342b386b
Vladimir Sabev <vdsabev@gmail.com>
2012-07-04 21:45:23 +0300

    Increased maximum post length

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,7 +10,7 @@ class Post < ActiveRecord::Base
   
   validates :content,
     :presence => true,
-    :length => { :maximum => 1000 }
+    :length => { :maximum => 10000 }
   validates :user_id,
     :presence => true
   
